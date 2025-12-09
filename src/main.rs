b34c0fd601db20687060221facf9c006c94d8301
羽山 <linainverseshadow@gmail.com>
2025-12-09 22:43:39 +0800

    簽入 php.rs 初步完成

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
+mod php;
 use headless_chrome::{Browser, LaunchOptions, protocol::cdp::Page};
 use std::ffi::OsString;
 use std::{env, error::Error, fs::File, io::Write, thread, time::Duration};
-fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
 
-    if args.len() != 6 {
-        println!("Usage: shot <URL> <WIDTH> <HEIGHT> <DELAY_MS> <OUTPUT_FILE>");
-        println!(
-            r#"Example:
-screenshot_rust "https://example.com" 1280 720 1500 "out.png""#
-        );
-        return Ok(());
+fn myLog(s: &str) {
+    let pwd = env::current_dir().unwrap();
+    if (php::is_dir(&format!("{}\\log", pwd.to_str().unwrap())) == false) {
+        php::mkdir(&format!("{}\\log", pwd.to_str().unwrap()));
     }
-
-    let url = &args[1];
-    let width: u32 = args[2].parse()?;
-    let height: u32 = args[3].parse()?;
-    let delay_ms: u64 = args[4].parse()?;
-    let output_file = &args[5];
-
-    let mut launch_options = LaunchOptions::default_builder().build().expect("Couldn't find appropriate Chrome binary.");
-        //.headless(true)
-        //.build()
-        //.expect("Couldn't find appropriate Chrome binary.");
+    let t: u64 = php::time();
+    let log_file = format!(
+        "{}\\log\\log_{}.txt",
+        pwd.to_str().unwrap(),
+        php::date("Ymd", Some(t))
+    );
+    let mut data = String::new();
+    data.push_str(&format!("{}: {}\n", php::date("Y-m-d H:i:s", Some(t)), s));
+    println!("{}", data);
+    php::file_put_contents(&log_file, &data, true);
+}
+fn screenshot(
+    url: &str,
+    width: u32,
+    height: u32,
+    delay_ms: u64,
+    output_file: &str,
+) -> Result<(), Box<dyn Error>> {
+    let mut launch_options = LaunchOptions::default_builder()
+        .build()
+        .expect("Couldn't find appropriate Chrome binary.");
+    //.headless(true)
+    //.build()
+    //.expect("Couldn't find appropriate Chrome binary.");
+    launch_options.path = Some("D:\\tools\\chrome-win\\chrome.exe".into());
     launch_options.headless = true;
     launch_options.enable_gpu = false;
     launch_options.window_size = Some((width, height));
@@ -30,73 +40,108 @@ screenshot_rust "https://example.com" 1280 720 1500 "out.png""#
     launch_options.devtools = false;
     launch_options.ignore_certificate_errors = true;
     launch_options.enable_logging = false;
-    // 設定視窗大小，必須透過 args 傳給 Chromium
-    //let mut wsss = format!("--window-size={}x{}", width, height);
 
-    //let mut os_string1 = OsString::from(format!("--headless"));
-    //launch_options.args.push(&os_string1);
-    //let mut os_string2 = OsString::from(format!("--window-size={}x{}", width, height));
-    //launch_options.args.push(&os_string2);
-    /*let mut os_string3 = OsString::from(format!("--disable-gpu"));
-    launch_options.args.push(&os_string3);
-    let mut os_string4 = OsString::from(format!("--remote-debugging-port=0"));
-    launch_options.args.push(&os_string4);
-    let mut os_string5 = OsString::from(format!("--no-sandbox"));
-    launch_options.args.push(&os_string5);
-    let mut os_string6 = OsString::from(format!("--headless"));
-    launch_options.args.push(&os_string6);
-    */
-    //launch_options.args.push(OsString::from("--headless=new")); // 強制新版 headless
-    //launch_options.args.push(OsString::from("--disable-gpu"));
-    //launch_options.args.push(OsString::from("--no-sandbox"));
     let browser = Browser::new(launch_options)?;
 
     let tab = browser.new_tab()?;
 
-    /// Navigate to wikipedia
     tab.navigate_to(url)?;
     tab.wait_until_navigated()?;
-    /// Wait for network/javascript/dom to make the search-box available
-    /// and click it.
-    //tab.wait_for_element("input#searchInput")?.click()?;
 
-    /// Type in a query and press `Enter`
-    //tab.type_str("WebKit")?.press_key("Enter")?;
-    // Delay（等待 JS/動畫/DOM 穩定）
+    // 等待指定的延遲時間
     thread::sleep(Duration::from_millis(delay_ms));
 
-    /// We should end up on the WebKit-page once navigated
-    //let elem = tab.wait_for_element("#firstHeading")?;
-    //assert!(tab.get_url().ends_with("WebKit"));
+    // 取得頁面高度
+    let _page_height: f64 = tab
+        .evaluate(
+            "Math.max(document.body.scrollHeight, document.documentElement.scrollHeight)",
+            false,
+        )?
+        .value
+        .unwrap()
+        .as_f64()
+        .unwrap();
 
-    /// Take a screenshot of the entire browser window
+    // 隱藏捲軸
+    tab.evaluate(r#"document.body.style.overflow = 'hidden';"#, false)?;
+
     let _png_data =
         tab.capture_screenshot(Page::CaptureScreenshotFormatOption::Png, None, None, true)?;
 
     let mut file = File::create(output_file)?;
     file.write_all(&_png_data)?;
 
-    /*
-    /// Take a screenshot of just the WebKit-Infobox
-    let _png_data = tab
-        .wait_for_element("#mw-content-text > div > table.infobox.vevent")?
-        .capture_screenshot(Page::CaptureScreenshotFormatOption::Png)?;
-
-    // Run JavaScript in the page
-    let remote_object = elem.call_js_fn(r#"
-        function getIdTwice () {
-            // `this` is always the element that you called `call_js_fn` on
-            const id = this.id;
-            return id + id;
-        }
-    "#, vec![], false)?;
-    match remote_object.value {
-        Some(returned_string) => {
-            dbg!(&returned_string);
-            assert_eq!(returned_string, "firstHeadingfirstHeading".to_string());
-        }
-        _ => unreachable!()
-    };
-    */
     Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let lockfile = format!("{}\\lock.txt", php::basedir());
+    println!("lockfile: {}", lockfile);
+    if (php::is_file(&lockfile) == false) {
+        php::touch(&lockfile);
+    }
+    // 先檢查
+    if (php::is_lock(&lockfile) == true) {
+        println!("File is locked, skip processing.");
+        return Ok(());
+    }
+
+    // 加鎖
+    let _lf = php::lock_file(&lockfile)?;
+    println!("File locked, processing...");
+
+    loop {
+        let now = chrono::Local::now();
+        let files = php::glob_files("D:\\web\\109_john_webservice\\works\\screenshot\\*.json");
+        for f in files {
+            let content = std::fs::read_to_string(&f)?;
+            // remove json file
+            php::unlink(&f);
+            // {F
+            //    "URL":"https://fmg.wra.gov.tw/109wraweb/fmg_fmgb_status.html?_t=1765287690",
+            //    "WIDTH":"970",
+            //    "HEIGHT":"640",
+            //    "DELAY":"20000",
+            //    "OUTPUT_FILE":"D:\\web\\109_john_webservice\\cache\\fmg_fmgb_img\\fmg_fmgb_1765287635.png"
+            // }
+            let config: serde_json::Value = serde_json::from_str(&content)?;
+
+            let url = config["URL"].as_str().unwrap_or("");
+            let width = config["WIDTH"].as_u64().unwrap_or(1280) as u32;
+            let height = config["HEIGHT"].as_u64().unwrap_or(720) as u32;
+            let delay_ms = config["DELAY"].as_u64().unwrap_or(1000);
+            let output_file = config["OUTPUT_FILE"].as_str().unwrap_or("screenshot.png");
+
+            let timestamp = now.format("%Y%m%d_%H%M%S").to_string();
+
+            println!("Taking screenshot of {} ...", url);
+            myLog(&format!("Taking screenshot of {} ...", url));
+            match screenshot(url, width, height, delay_ms, &output_file) {
+                Ok(_) => println!("Screenshot saved to {}", output_file),
+                Err(e) => eprintln!("Error taking screenshot: {}", e),
+            }
+            if (php::is_file(&output_file)) {
+                let file_size = php::filesize(&output_file);
+                println!("File size: {} bytes", file_size);
+                myLog(&format!(
+                    "Screenshot taken: {}, size: {} bytes",
+                    output_file, file_size
+                ));
+            } else {
+                println!("Error...File not found: {}", output_file);
+            }
+        }
+        // sleep 1 second
+        thread::sleep(Duration::from_secs(1));
+
+        if (php::is_file("D:\\tools\\114screenshot_rust\\restart.txt")) {
+            php::unlink("D:\\tools\\114screenshot_rust\\restart.txt");
+            myLog("Restarting...");
+            println!("Restarting...");
+            php::exit();
+            break;
+        }
+        // println!("...");
+    }
+    return Ok(());
 }
